5ef14cf8314faa1c6b8df423cbbc9886560fbd16
mesiarm <mesiarm@gmail.com>
2021-03-26 01:01:41 +0100

    fix props, comments, td

--- assets/components/ReservationTable.tsx
+++ assets/components/ReservationTable.tsx
@@ -1,10 +1,7 @@
 import * as React from "react";
 import {MDBTable, MDBTableBody, MDBTableHead} from "mdbreact";
 
-type ReservationTableProps = {
-    // using `interface` is also ok
-    message: string;
-};
+type ReservationTableProps = {};
 type Day = {
     id: number,
     name: string,
@@ -50,9 +47,6 @@ class ReservationTable extends React.Component<ReservationTableProps, Reservatio
                         reservations: result
                     });
                 },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
                 (error) => {
                     this.setState({
                         isLoaded: true,
@@ -68,9 +62,6 @@ class ReservationTable extends React.Component<ReservationTableProps, Reservatio
                         days: result
                     });
                 },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
                 (error) => {
                     this.setState({
                         isLoaded: true,
@@ -81,15 +72,11 @@ class ReservationTable extends React.Component<ReservationTableProps, Reservatio
         fetch("http://hairsalon.local/intervals")
             .then(res => res.json())
             .then((result) => {
-                    console.log(result);
                     this.setState({
                         isLoaded: true,
                         intervals: result
                     });
                 },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
                 (error) => {
                     this.setState({
                         isLoaded: true,
@@ -121,15 +108,16 @@ class ReservationTable extends React.Component<ReservationTableProps, Reservatio
                             <tr key={day.id}>
                                 <td>{day.name}</td>
                                 {S.intervals.map(interval => {
-                                    console.log(S.reservations.length > 0);
-                                        const reserved = S.reservations.length > 0 ? S.reservations.some(r => {
-                                                return r.interval.id === interval.id &&
-                                                    r.day.id === day.id;
-                                            }
-                                        ): false;
+                                        const reserved = S.reservations.length > 0
+                                            ? S.reservations.some(r => {
+                                                    return r.interval.id === interval.id &&
+                                                        r.day.id === day.id;
+                                                }
+                                            )
+                                            : false;
                                         const color = reserved ? 'danger-color' : 'success-color';
                                         const text = reserved ? 'Obsadené' : 'Voľné';
-                                        return <th key={interval.id} className={color}>{text}</th>;
+                                        return <td key={interval.id} className={color}>{text}</td>;
                                     }
                                 )}
                             </tr>
